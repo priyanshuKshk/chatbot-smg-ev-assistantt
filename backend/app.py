d849f64befc0d6a9c32a44194ca2535c38e4679a
priyanshuKshk <kshkpriyanshu64@gmail.com>
2025-04-23 21:56:06 +0530

    routing properly

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_cors import CORS
 from backend.routes import chatbot_route
-
+from flask import send_from_directory
 import sys
 import os
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
@@ -14,3 +14,15 @@ app.register_blueprint(chatbot_route)
 
 if __name__ == "__main__":
     app.run(debug=True)  # Allow access from local network
+@app.route("/")
+def home():
+    return "Backend is running"
+@app.route('/')
+def serve_frontend():
+    return send_from_directory('../frontend/build', 'index.html')
+@app.route('/')
+def serve_react():
+    return send_from_directory('../frontend/build', 'index.html')
+@app.route('/<path:path>')
+def serve_static_file(path):
+    return send_from_directory('../frontend/build', path)
