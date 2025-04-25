f1ef3f559c7c30da83f84935c352a6f47c217cd2
priyanshuKshk <kshkpriyanshu64@gmail.com>
2025-04-26 01:16:58 +0530

    url backend

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -9,7 +9,7 @@ const isLocalhost = window.location.hostname === "localhost";
 const config = {
   API_URL: isLocalhost
     ? "http://localhost:5000"         
-    : "https://smg-backend.onrender.com", 
+    : "https://chatbot-smg-ev-assistant.onrender.com", 
 };
 
 export default config;
